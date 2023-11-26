2b243a489368020f2c3d7a110ffab9a87f01048a
Hakan KARAYILMAZ <hkankyilmazz@gmail.com>
2023-11-26 14:59:45 +0300

    mobile navbar updated

--- app/_components/Header/Index.js
+++ app/_components/Header/Index.js
@@ -61,43 +61,43 @@ export default function Header() {
         <header onMouseEnter={onEnter} onMouseLeave={onLeave} ref={ref} className={`${styles.container} shadow-lg py-2 fixed left-0 top-0 right-0 z-10 max-md:h-[70px] overflow-hidden max-md:bg-white max-md:bottom-0 max-md:flex max-md:flex-col max-md-justify-center max-md:items-start`}>
             <a className='flcenter' href={"/"} >  <div className='absolute max-md:left-1 top-[20px] left-0 text-black z-10 !flex flcenter'> <Image className='translate-y-[-8px] max-md:translate-x-[-30px] max-md:w-[175px] ' width={200} src={logo} /> </div></a>
             <div ref={ref_} className='max-md:hidden flex justify-center items-start max-md:m-auto max-md:flex-col max-md-justify-start max-md:items-start max-md:[&>a]:mb-5' >
-                <Link onClick={() => handleClickClose()} className='flex flex-col max-md:items-start justify-center items-center m-link' href={"/arac"} >
-                    <Image className={`max-w-[50px] ${styles.fimg}`} src={pathname == "/car" ? car : car_} />
-                    <Image className={`max-w-[50px] hidden ${styles.simg}`} src={car} />
+                <Link onClick={() => handleClickClose()} className='flex flex-col max-md:items-start justify-center items-center m-link min-w-[120px]' href={"/arac"} >
+                    <Image className={`m-auto max-w-[50px] ${styles.fimg}`} src={pathname == "/car" ? car : car_} />
+                    <Image className={`m-auto max-w-[50px] hidden ${styles.simg}`} src={car} />
                     <div className='menu-item md:absolute md:top-[65%]  text-sx' >
-                        <div>Arabam</div>
+                        <div className='max-md:mr-1' >Arabam</div>
                         <div>Sigortalı</div>
                     </div>
                 </Link>
-                <Link onClick={() => handleClickClose()} className='flex flex-col max-md:items-start justify-center items-center m-link' href={"/ev"} >
-                    <Image className={`max-w-[50px] ${styles.fimg}`} src={pathname == "/ev" ? home : home_} />
-                    <Image className={`max-w-[50px] hidden ${styles.simg}`} src={home} />
+                <Link onClick={() => handleClickClose()} className='flex flex-col max-md:items-start justify-center items-center m-link min-w-[120px]' href={"/ev"} >
+                    <Image className={`m-auto max-w-[50px] ${styles.fimg}`} src={pathname == "/ev" ? home : home_} />
+                    <Image className={`m-auto max-w-[50px] hidden ${styles.simg}`} src={home} />
                     <div className='menu-item md:absolute md:top-[65%] text-sx' >
-                        <div> Evim </div>
+                        <div className='max-md:mr-1'> Evim </div>
                         <div>Sigortalı</div>
                     </div>
                 </Link>
-                <Link onClick={() => handleClickClose()} className='flex flex-col max-md:items-start justify-center items-center m-link' href={"/isyeri"}>
-                    <Image className={`max-w-[50px] ${styles.fimg}`} src={pathname == "/isyeri" ? company : company_} />
-                    <Image className={`max-w-[50px] hidden ${styles.simg}`} src={company} />
+                <Link onClick={() => handleClickClose()} className='flex flex-col max-md:items-start justify-center items-center m-link min-w-[120px]' href={"/isyeri"}>
+                    <Image className={`m-auto max-w-[50px] ${styles.fimg}`} src={pathname == "/isyeri" ? company : company_} />
+                    <Image className={`m-auto max-w-[50px] hidden ${styles.simg}`} src={company} />
                     <div className='menu-item md:absolute md:top-[65%] text-sx'>
-                        <div> İşyerim </div>
+                        <div className='max-md:mr-1'> İşyerim </div>
                         <div>Sigortalı</div>
                     </div>
                 </Link>
-                <Link onClick={() => handleClickClose()} className='flex flex-col max-md:items-start justify-center items-center m-link' href={"/seyahat"}>
-                    <Image className={`max-w-[50px] ${styles.fimg}`} src={pathname == "/seyahat" ? trip : trip_} />
-                    <Image className={`max-w-[50px] hidden ${styles.simg}`} src={trip} />
+                <Link onClick={() => handleClickClose()} className='flex flex-col max-md:items-start justify-center items-center m-link min-w-[120px]' href={"/seyahat"}>
+                    <Image className={`m-auto max-w-[50px] ${styles.fimg}`} src={pathname == "/seyahat" ? trip : trip_} />
+                    <Image className={`m-auto max-w-[50px] hidden ${styles.simg}`} src={trip} />
                     <div className='menu-item md:absolute md:top-[65%] text-sx'>
-                        <div> Seyahatim </div>
+                        <div className='max-md:mr-1'> Seyahatim </div>
                         <div>Sigortalı</div>
                     </div>
                 </Link>
-                <Link onClick={() => handleClickClose()} className='flex flex-col max-md:items-start justify-center items-center m-link' href={"/saglik"} >
-                    <Image className={`max-w-[50px] ${styles.fimg}`} src={pathname == "/saglik" ? heart : heart_} />
-                    <Image className={`max-w-[50px] hidden ${styles.simg}`} src={heart} />
+                <Link onClick={() => handleClickClose()} className='flex flex-col max-md:items-start justify-center items-center m-link min-w-[120px]' href={"/saglik"} >
+                    <Image className={`m-auto max-w-[50px] ${styles.fimg}`} src={pathname == "/saglik" ? heart : heart_} />
+                    <Image className={`m-auto max-w-[50px] hidden ${styles.simg}`} src={heart} />
                     <div className='menu-item md:absolute md:top-[65%]'>
-                        <div> Sağlığım </div>
+                        <div className='max-md:mr-1'> Sağlığım </div>
                         <div>Sigortalı</div>
                     </div>
                 </Link>
