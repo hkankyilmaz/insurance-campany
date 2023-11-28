35ea022577f3a3e11c14cbdf4c31fdb341f65177
Hakan KARAYILMAZ <hkankyilmazz@gmail.com>
2023-11-28 23:08:42 +0300

    Update SectionThree.js

--- app/_components/Sections/SectionThree.js
+++ app/_components/Sections/SectionThree.js
@@ -7,7 +7,7 @@ import { motion } from 'framer-motion';
 
 function SectionThree() {
     return (
-        <section className='w-[100vw] md:pt-[100px] max-md:py-[0px] px-[25px] lg:px-[200px] xl:px-[300px] flex flex-col justify-start items-center relative max-md:bg-slate-50' >
+        <section className='w-[100vw] md:pt-[100px] max-md:py-[0px] px-[25px] lg:px-[200px] xl:px-[300px] flex flex-col justify-start items-center relative' >
             <p className='font-bold text-2xl max-md:text-lg my-4' >Nasıl Çalışır?</p>
             <motion.div
                 className='max-md:flex-col  md:flcenter'
@@ -16,7 +16,10 @@ function SectionThree() {
                 viewport={{ once: true }}
                 transition={{ duration: 0.5 }}
             >
-                <div className=' md:mr-20'  >
+                <video className='md:hidden w-[300px] max-md:w-[200px] max-md:m-auto' ty src={"https://firebasestorage.googleapis.com/v0/b/kutezadmin.appspot.com/o/hakan%2Fakdag%CC%86c%C4%B1%20sigorta%20kopyas%C4%B1.mp4?alt=media&token=951223ea-6abd-4da9-82c1-3c9f1cb21c3f&_gl=1*1kixezl*_ga*MTcyMjMzNjU0MC4xNjk2MzIzNDE3*_ga_CW55HF8NVT*MTY5Njc3MDEwMC43LjEuMTY5Njc3MDIxOC4zNy4wLjA."} style={{ viewTransitionName: "visible" }} muted autoPlay loop playsInline >
+                    <track kind='caption' />
+                </video>
+                <div className=' md:mr-20 max-md:mt-7'  >
                     <div className='flex items-center  mb-10'>
                         <div className='mb-2 mr-4 md:mr-6'>  <div className='w-20 h-20 flcenter max-md:w-10 max-md:h-10 border rounded-full' ><BsSendFill className='max-md:text-xl text-4xl' /></div>  </div>
                         <div>
