34b7fe6f51fbe3e23ee3126a711af4788f7191c1
Hakan KARAYILMAZ <hkankyilmazz@gmail.com>
2024-01-02 01:39:38 +0300

    dask nedir blog

---
+++ app/blog/dask-nedir-daskin-onemi/page.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import Image from 'next/image'
+import Footer from '@/app/_components/Footer/Index'
+
+function Dask() {
+    return (
+        <>
+            <div className='flex justify-center items-start pt-[100px] pb-[100px] max-lg:px-7'>
+
+                <div className='max-w-[1000px] grid grid-cols-1 gap-2' >
+                    <strong></strong>
+
+                    <h2 className='text-xl'> <strong>DASK Nedir? DASK’ın Önemi Nedir?</strong>  </h2>
+                    <div className='grid grid-cols-1 gap-4'>
+
+                        <p>
+                            Ev sahibi olmak, hayatımızın en önemli kararlarından biridir ve bu kararı verirken bir dizi önemli faktörü göz önünde bulundurmak gerekir. Bu faktörlerin başında ise doğal afetlere karşı alınacak tedbirler ve doğru sigorta seçimi gelmektedir. Türkiye'nin deprem kuşağında yer alması, bu konuda daha bilinçli ve özenli adımlar atmamızı gerektirir. İşte bu noktada devreye, konut sahiplerine maddi güvence sağlayan
+                            <strong> DASK (Doğal Afet Sigortası Kurumu)</strong> girer.
+                        </p>
+                        <p>
+                            <strong>DASK,</strong>  Türkiye'de konut sahiplerine doğal afetlerden (deprem, sel, heyelan vb.) kaynaklanan maddi kayıplara karşı koruma sağlayan bir sigorta kurumudur. Bu sigorta, doğal afetlerin oluşturabileceği maddi hasarı en aza indirmek amacıyla devlet tarafından desteklenir ve ev sahiplerine güvence sağlar. Özellikle ülkemizde sıklıkla yaşanan depremler göz önüne alındığında, DASK'ın ne kadar kritik bir öneme sahip olduğunu anlamak mümkündür.
+                            DASK’ın hayatımıza kattığı avantajlara bakacak olursak;
+                        </p>
+                        <p>
+                            1. <strong>   Zorunlu Güvence:</strong> Türkiye'de konut alımı gerçekleştirildiğinde, DASK otomatik olarak devreye girer. Bu, ev sahiplerini deprem riskine karşı koruma altına alır ve zorunlu bir güvence sağlar.
+                        </p>
+                        <p>
+                            2. <strong>  Geniş Kapsam:</strong> DASK, sadece deprem değil, aynı zamanda sel, heyelan gibi doğal afetlerin neden olduğu zararları da kapsar. Bu, ev sahiplerine kapsamlı bir güvence sunar.
+                        </p>
+                        <p>
+                            3. <strong>  Ekonomik Koruma:</strong> Doğal afetlerin neden olduğu maddi hasarlar, ciddi finansal yükler getirebilir. DASK, ev sahiplerini beklenmedik durumlar karşısında ekonomik olarak koruma altına alır.
+                        </p>
+                        <p>
+                            DASK’ın Amacı:
+                        </p>
+                        <p>
+                            - Yurtiçindeki tüm evlerin güvence altına alınması
+                        </p>
+                        <p>
+                            - Yurtiçinde risk paylaşımının düzgün bir biçimde yapılması
+                        </p>
+                        <p>
+                            - Depremin ülkemize getireceği mali zararın sigorta yoluyla, uluslararası reasürans (yeniden sigortalama) ve sermaye piyasalarına dağılması
+                        </p>
+                        <p>
+                            - Devletin deprem ve sonrası oluşacak hasarlarda mali yükünün azaltılması
+                        </p>
+                        <p>
+                            - Sağlık yapıların oluşturulması için teşvik sağlanması
+                        </p>
+                        <p>
+                            - Deprem hasarlarının karşılanması için uzun vadeli kaynak birikiminin sağlanması
+                        </p>
+                        <p>
+                            - Toplumsal sigorta ve sosyal dayanışma bilincinin oluşturulması
+                        </p>
+                        <p>
+                            Deprem bölgesi olan Türkiye’de bu bilinçlerin edinilmesi ve bu gerçekle yaşanabilmesi bu önlemlerin alınması çok önemlidir. Evinizi güvence altına almanın ve olası doğal afetlerin getirebileceği maddi kayıplardan korunmanın en etkili yolu, doğru sigorta seçimini yapmaktır. DASK, bu konuda sizin için bir adım öne çıkar ve ev sahiplerine beklenmedik durumlar karşısında finansal koruma sağlar. Unutmayın, doğru sigorta seçimiyle evinizdeki değeri güvence altına alabilir ve geleceğe daha emin adımlarla ilerleyebilirsiniz.
+                        </p>
+                        <p>
+
+                            Size özel avantajlı fiyat teklifleri veya daha fazla bilgi edinmek için bizimle iletişime geçebilirsiniz!
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+            <Footer />
+        </>
+    )
+}
+
+export default Dask
